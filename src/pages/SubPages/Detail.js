cb73b904e8d7a8db3489d474f8e74f3fa812571c
wjs5025 <wjs_5025@naver.com>
2021-11-16 15:59:25 +0900

    코멘트 추가

--- src/pages/SubPages/Detail.js
+++ src/pages/SubPages/Detail.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import FloatingBtn from "../../components/FloatingBtn";
+import Comment from "../../assets/Images/comment2.png";
 
 const Detail = () => {
   const history = useHistory();
@@ -18,8 +19,30 @@ const Detail = () => {
       idx: "",
     },
   ]);
-  const [date, setDate] = useState("");
 
+  // 댓글
+  const [date, setDate] = useState("");
+  const [CommentSet, setCommentSet] = useState({
+    data: [],
+  });
+  const [cmtOpen, setCmtOpen] = useState(true);
+  const [cmtClassName, setCmtClassName] = useState("");
+  const [cmtRender, setCmtRender] = useState("");
+
+  const CommentState = () => {
+    if (cmtOpen == true) {
+      setCmtOpen(false);
+      setCmtClassName("");
+
+      setCmtRender("cmtRenderFalse");
+    } else {
+      setCmtOpen(true);
+      setCmtClassName("CmtClose");
+      setCmtRender("cmtRenderTrue");
+    }
+  };
+
+  useEffect(CommentState, []);
   //게시글 경로 확인
   const pathArray = history.location.pathname.split("/");
   const BoardPath = pathArray[2];
@@ -41,6 +64,22 @@ const Detail = () => {
 
   useEffect(getData, []);
 
+  const getComment = () => {
+    axios
+      .get("http://172.18.3.25:3001/Comment", {
+        params: {
+          BoardPath: BoardPath,
+          PostNum: PostNum,
+        },
+      })
+      .then((res) => {
+        setCommentSet({ ...CommentSet, data: res.data });
+      });
+  };
+
+  useEffect(getComment, []);
+
+  const { data } = CommentSet;
   return (
     <>
       <div className="Forum_container">
@@ -65,6 +104,32 @@ const Detail = () => {
         <div className="Detail_contents">
           <pre>{dataSet.contents}</pre>
           <FloatingBtn history={history} pathArray={pathArray} />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            margin: "15px 0 0 10px",
+            alignItems: "center",
+          }}
+          onClick={CommentState}
+        >
+          <img
+            src={Comment}
+            className={cmtClassName}
+            style={{ transition: "100ms" }}
+            width={15}
+            height={15}
+          />
+          <h5 style={{ margin: "0 0 0 10px", fontFamily: "SCDream6" }}>댓글</h5>
+        </div>
+        <div className={cmtRender} style={{ margin: "10px 0 0 10px" }}>
+          {CommentSet.data.map((nowComment) => (
+            <tr key={nowComment.idx}>
+              <td>{nowComment.writer}/</td>
+              <td>{nowComment.contents}/</td>
+              <td> {nowComment.date.substr(0, 10)}</td>
+            </tr>
+          ))}
         </div>
       </div>
     </>
